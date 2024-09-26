d89bd7d95370c3e5059d59b5d76d95b03282ed38
Grzegorz Brzeczek <gbrzeczek@dtiq.com>
2024-09-26 18:42:52 +0200

    chore: migrated to lua config

---
+++ init.lua
@@ -0,0 +1,81 @@
+-- Set options
+vim.opt.compatible = false
+vim.opt.showmatch = true
+vim.opt.ignorecase = true
+vim.opt.mouse = 'a'
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.number = true
+vim.opt.wildmode = 'longest,list'
+vim.opt.cc = '120'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.cursorline = true
+vim.opt.ttyfast = true
+vim.opt.relativenumber = true
+
+-- Set leader key
+vim.g.mapleader = ','
+
+-- Enable filetype plugins
+vim.cmd('filetype plugin indent on')
+vim.cmd('syntax on')
+
+-- Plugin management with vim-plug
+vim.cmd([[
+call plug#begin()
+Plug 'github/copilot.vim'
+Plug 'tiagovla/tokyodark.nvim'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.5' }
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+Plug 'dense-analysis/ale'
+Plug 'nvim-neo-tree/neo-tree.nvim', {'branch': 'v2.x', 'do': ':UpdateRemotePlugins'}
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-tree/nvim-web-devicons'
+Plug 'MunifTanjim/nui.nvim'
+Plug 'neovim/nvim-lspconfig'
+Plug 'lewis6991/gitsigns.nvim'
+Plug 'nvim-tree/nvim-web-devicons'
+Plug 'romgrk/barbar.nvim'
+call plug#end()
+]])
+
+-- Set colorscheme
+vim.cmd('colorscheme tokyodark')
+
+-- Keybindings
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Telescope bindings
+map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
+map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
+map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts)
+map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', opts)
+
+-- Neotree binding
+map('n', '<Leader>n', ':Neotree toggle<CR>', opts)
+
+-- Barbar bindings
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<A-S-c>', '<Cmd>BufferRestore<CR>', opts)
+
